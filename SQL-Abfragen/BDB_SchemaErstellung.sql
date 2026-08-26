786a5d537e1152e57d0e559bc08d749f09bdd07a
snavc <van.stiphoudt.christine@rwth-aachen.de>
2026-08-26 13:57:50 +0200

    Fix syntax errors

--- SQL-Abfragen/BDB_SchemaErstellung.sql
+++ SQL-Abfragen/BDB_SchemaErstellung.sql
@@ -1,7 +1,6 @@
-
-
 --- Erstelle Verwaltungsdatenbankschema
 CREATE SCHEMA bdb_Verwaltung;
+GO
 
 CREATE TABLE bdb_Verwaltung.Adresse(
 	AdresseID INT Identity(1,1) NOT NULL PRIMARY Key,
@@ -10,9 +9,11 @@ CREATE TABLE bdb_Verwaltung.Adresse(
 	Ort NVARCHAR(2) NOT NULL,
 	Hausnummer NVARCHAR(7)  NOT NULL
 );
+GO
 
 --- Erstelle Personaldatenbankschema
 CREATE SCHEMA bdb_Personal;
+GO
 
 CREATE TABLE bdb_Personal.Mitarbeiter (
 	MitarbeiterID INT Identity(1,1) NOT NULL PRIMARY Key,
@@ -26,9 +27,11 @@ CREATE TABLE bdb_Personal.Mitarbeiter (
 	AdresseID INTEGER NOT NULL FOREIGN KEY REFERENCES bdb_Verwaltung.Adresse(AdresseID),
 	Arbeitsbereich NVARCHAR(20) NOT NULL
 );
+GO
 
 --- Erstelle Immobilienverwaltungsschema
 CREATE SCHEMA bdb_Immobilienverwaltung;
+GO
 
 CREATE TABLE bdb_Immobilienverwaltung.Filiale(
 	FilialID INT Identity(1,1) NOT NULL PRIMARY Key,
@@ -40,9 +43,71 @@ CREATE TABLE bdb_Immobilienverwaltung.Filiale(
 	sonstNebenkosten Decimal(7,2) NOT NULL,
 	GrößeInQm Decimal(7,2) NOT NULL
 );
+GO
+
+
+--- Erstelle Kassensystem
+CREATE SCHEMA bdb_Kassensystem;
+GO
+
+CREATE TABLE bdb_Kassensystem.Verkäufer(
+	VerkäuferID INT NOT NULL PRIMARY KEY,
+    MitarbeiterID INT NOT NULL,
+	FOREIGN KEY(MitarbeiterID) REFERENCES bdb_Personal.Mitarbeiter(MitarbeiterID)
+);
+
+CREATE TABLE bdb_Kassensystem.Verkauf(
+	Zeitpunkt DATETIME2 NOT NULL PRIMARY KEY,
+    FilialID INT NOT NULL,
+    Zahlart CHAR(1),
+    VerkäuferID INT NOT NULL,
+    FOREIGN KEY (FilialID) REFERENCES bdb_Immobilienverwaltung.Filiale(FilialID),
+    FOREIGN KEY (VerkäuferID) REFERENCES bdb_Kassensystem.Verkäufer(VerkäuferID)
+);
+
+CREATE TABLE bdb_Kassensystem.Kategorie(
+	KategorieID INT NOT NULL PRIMARY KEY,
+    Name NVARCHAR(50) NOT NULL
+);
+CREATE TABLE bdb_Kassensystem.Produkt(
+	ProduktID INT NOT NULL PRIMARY KEY,
+    Bruttopreis DECIMAL(4,2) NOT NULL,
+    Nettopreis DECIMAL(4,2) NOT NULL,
+    ProduktName NVARCHAR(50) NOT NULL,
+    KategorieID INT NOT NULL,
+    FOREIGN KEY (KategorieID) REFERENCES bdb_Kassensystem.Kategorie(KategorieID)
+);
+
+
+CREATE TABLE bdb_Kassensystem.arbeitetIn(
+	SchichtID INT NOT NULL,
+	FilialID INT NOT NULL,
+	VerkäuferID INT NOT NULL,
+    Datum DATE NOT NULL,
+	Schichtende time NOT NULL, 
+	Schichtbeginn time NOT NULL,
+    PRIMARY KEY (SchichtID), 
+    FOREIGN KEY (FilialID) REFERENCES bdb_Immobilienverwaltung.Filiale(FilialID),
+    FOREIGN KEY (VerkäuferID) REFERENCES bdb_Kassensystem.Verkäufer(VerkäuferID)
+
+);
+
+CREATE TABLE bdb_Kassensystem.wirdVerkauft(
+	FilialID INT NOT NULL,
+    Zeitpunkt DATETIME2 NOT NULL,
+    ProduktID INT NOT NULL,
+    Menge DECIMAL(5,2),
+    PRIMARY KEY (FilialID, Zeitpunkt, ProduktID),
+    FOREIGN KEY (FilialID) REFERENCES bdb_Immobilienverwaltung.Filiale(FilialID),
+    FOREIGN KEY (ProduktID) REFERENCES bdb_Kassensystem.Produkt(ProduktID),
+	FOREIGN KEY (Zeitpunkt) REFERENCES bdb_Kassensystem.Verkauf(Zeitpunkt)
+);
+GO
+
 
 --- Erstelle Rezepteschema
 CREATE SCHEMA bdb_Rezepte;
+GO
 
 CREATE TABLE bdb_Rezepte.Rezept(
 	ProduktID INTEGER NOT NULL,
@@ -53,7 +118,7 @@ CREATE TABLE bdb_Rezepte.Rezept(
 	Backzeit Decimal(4,2) NOT NULL,
 	Autor NVARCHAR(50) NOT NULL,
 	Stückzahl TINYINT,
-	PRIMARY KEY (ProduktID, RezeptVersion)
+	PRIMARY KEY (ProduktID, RezeptVersion),
 	FOREIGN KEY (ProduktID) REFERENCES bdb_Kassensystem.Produkt(ProduktID)
 );
 
@@ -75,61 +140,4 @@ CREATE TABLE bdb_Rezepte.beinhatet(
 	FOREIGN KEY (ZutatID) REFERENCES bdb_Rezepte.Zutat(ZutatID),
 	FOREIGN KEY (ProduktID, RezeptVersion) REFERENCES bdb_Rezepte.Rezept(ProduktID, RezeptVErsion)
 );
-
---- Erstelle Kassensystem
-CREATE SCHEMA bdb_Kassensystem;
-
-CREATE TABLE bdb_Kassensystem.Verkauf(
-	Zeitpunkt DATETIME2 NOT NULL PRIMARY KEY,
-    FilialID INT NOT NULL,
-    Zahlart CHAR(1),
-    VerkäuferID INT NOT NULL,
-    FOREIGN KEY (FilialID) REFERENCES bdb_Immobilienverwaltung.Filiale(FilialID),
-    FOREIGN KEY (VerkäuferID) REFERENCES bdb_Kassensystem.Verkäufer(VerkäuferID)
-);
-
-CREATE TABLE bdb_Kassensystem.Verkäufer(
-	VerkäuferID INT NOT NULL PRIMARY KEY,
-    MitarbeiterID INT NOT NULL,
-	FOREIGN KEY(MitarbeiterID) REFERENCES bdb_Personal.Mitarbeiter(MitarbeiterID)
-);
-
-CREATE TABLE bdb_Kassensystem.Produkt(
-	ProduktID INT NOT NULL PRIMARY KEY,
-    Bruttopreis DECIMAL(4,2) NOT NULL,
-    Nettopreis DECIMAL(4,2) NOT NULL,
-    ProduktName NVARCHAR(50) NOT NULL,
-    KategorieID INT NOT NULL,
-    FOREIGN KEY (KategorieID) REFERENCES bdb_Kassensystem.Kategorie(KategorieID)
-);
-
-CREATE TABLE bdb_Kassensystem.Kategorie(
-	KategorieID INT NOT NULL PRIMARY KEY,
-    Name NVARCHAR(50) NOT NULL
-);
-
-CREATE TABLE bdb_Kassensystem.arbeitetIn(
-	SchichtID INT NOT NULL,
-	FilialID INT NOT NULL,
-	VerkäuferID INT NOT NULL,
-    Datum DATE NOT NULL,
-	Schichtende time NOT NULL, 
-	Schichtbeginn time NOT NULL,
-    PRIMARY KEY (SchichtID), 
-    FOREIGN KEY (FilialID) REFERENCES bdb_Immobilienverwaltung.Filiale(FilialID)(FilialID),
-    FOREIGN KEY (VerkäuferID) REFERENCES bdb_Kassensystem.Verkäufert(VerkäuferID)
-
-);
-
-CREATE TABLE bdb_Kassensystem.wirdVerkauft(
-	FilialID INT NOT NULL,
-    Zeitpunkt DATETIME2 NOT NULL,
-    ProduktID INT NOT NULL,
-    Menge DECIMAL(5,2),
-    PRIMARY KEY (FilialID, Zeitpunkt, ProduktID),  -- Zusammensetzter Primärschlüssel
-    FOREIGN KEY (FilialID) REFERENCES bdb_Immobilienverwaltung.Filiale(FilialID)(FilialID),
-    FOREIGN KEY (ProduktID) REFERENCES bdb_Kassensystem.Produkt(ProduktID),
-	FOREIGN KEY (Zeitpunkt) REFERENCES bdb_Kassensystem.Verkauf(Zeitpunkt)
-);
-
-
+GO
