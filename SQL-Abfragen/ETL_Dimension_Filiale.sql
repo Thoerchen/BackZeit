a57b47c30c9e043fc5c818c608c9dbff0a0830d3
snavc <van.stiphoudt.christine@rwth-aachen.de>
2026-08-27 14:31:26 +0200

    Update ETL for Filiale as stored procedure

--- SQL-Abfragen/ETL_Dimension_Filiale.sql
+++ SQL-Abfragen/ETL_Dimension_Filiale.sql
@@ -1,6 +1,22 @@
 --- ETL für Dimension Filiale
 
 USE BDB_BackZeit;
+GO
+
+
+-- Stored Procedure entfernen, falls sie bereits existiert
+IF OBJECT_ID('sp_FillDimFiliale', 'P') IS NOT NULL
+    BEGIN
+        DROP PROCEDURE sp_FillDimFiliale;
+    END
+
+GO
+
+
+-- Stored Procedure erstellen
+CREATE PROCEDURE sp_FillDimFiliale
+AS
+BEGIN
 
 INSERT INTO DWH_BackZeit.dbo.D_Filiale (FilialID, FilialName, FilialOrt)
 SELECT
@@ -10,4 +26,23 @@ SELECT
 FROM
     BDB_BackZeit.bdb_Immobilienverwaltung.Filiale AS f
 JOIN
-    BDB_BackZeit.bdb_Verwaltung.Adresse AS a ON f.AdresseID = a.AdresseID;
+    BDB_BackZeit.bdb_Verwaltung.Adresse AS a ON f.AdresseID = a.AdresseID
+WHERE
+    NOT EXISTS
+        (SELECT 1
+        FROM DWH_BackZeit.dbo.D_Filiale AS df
+            WHERE df.FilialID = f.FilialID
+        );
+
+END
+
+GO
+
+-- Stored Procedure ausführen
+EXECUTE sp_FillDimFiliale;
+
+GO
+
+-- Ergebnis kontrollieren
+SELECT *
+FROM   DWH_BackZeit.dbo.D_Filiale;
